d05bdfa40f93e099412c2c671e202f017d727f42
Mir Lutfur Rahman <mirlutfur.rahman@gmail.com>
2020-01-24 23:18:26 +0600

    24/01/2019

---
+++ ow_admin/lib/order_processing_delivered.php
@@ -0,0 +1,20 @@
+<?php
+	//DB Connection update required for a new hosting
+	include("../../library/initialize.php");
+	if(isset($_REQUEST['change_status']) && isset($_REQUEST['order_id']))
+	{
+		$order_id=trim($_REQUEST['order_id']);
+		try
+		{
+			$stmt=$conn->prepare("update order_info set status='Delivered' where order_id=:order_id ");
+			$stmt->execute(array('order_id'=>$order_id));
+			
+		}catch(PDOException $e)
+		{
+			echo "Error: ".$e->getMessage();
+		}
+	
+	
+	}
+
+?>
